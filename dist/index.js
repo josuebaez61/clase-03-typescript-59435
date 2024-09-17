32df73968d8d51b4cd39a9fb9db3f34fa483836f
josue baez <josuebaez61@gmail.com>
2024-09-16 22:37:14 -0300

    ADD, clases

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Alumno = void 0;
 console.log("Hola mundo desde TS!");
 console.log("Este log ha sido traducido de TS a JS!");
 console.log("Nuevo log de consola, con el modo observador!");
@@ -14,4 +16,84 @@ let numerosYLetras2 = [1, "H", 3, "J"];
 function saludar(nombre) {
     console.log(`Hola ${nombre.toUpperCase()}!`);
 }
+function enviarMensaje(mensaje = "Valor por defecto") {
+    if (typeof mensaje === "string") {
+        mensaje.toUpperCase();
+    }
+}
+function sumar(a, b) {
+    return a + b;
+}
+const restar = (a, b) => a - b;
+// function despuesDe1Seg(accion: Function) {
+function despuesDe1Seg(accion) {
+    setTimeout(() => {
+        accion(5);
+    }, 1000);
+}
+despuesDe1Seg((a) => {
+    console.log(`Hola, recibimos: ${a}`);
+});
+enviarMensaje();
 saludar("Juan");
+let alumnos2 = [];
+let alumnos = [
+    { nombre: "Naruto", apellido: "Uzumaki", edad: 12, estudiar: () => { } },
+    {
+        nombre: "Sasuke",
+        apellido: "Uchiha",
+        edad: 12,
+        email: "sasuke@gmail.com",
+        estudiar: () => { },
+    },
+];
+let sakura = {
+    nombre: "Sakura",
+    apellido: "Haruno",
+    edad: 12,
+    estudiar: () => { },
+};
+function actualizarAlumnoById(id, update) { }
+function saludarAlumno(alumnoSinEdad) { }
+class Alumno {
+    constructor(nombre, apellido, edad, email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.email = email;
+    }
+    estudiar() {
+        console.log(`Hola, soy ${this.nombre} y estoy estudianto!`);
+    }
+}
+exports.Alumno = Alumno;
+const rockLee = new Alumno("Rock", "Lee", 12, "rocklee@mail.com");
+const peterParker = new Alumno("Peter", "Parker", 15, "peter@mail.com");
+peterParker.estudiar();
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+class Carrito {
+    constructor() {
+        this.productos = [];
+    }
+    calcularTotal() {
+        return this.productos.reduce((prev, next) => prev + next.precio, 0);
+    }
+    agregarAlCarrito(producto) {
+        this.productos.push(producto);
+    }
+}
+class MyEcommerce {
+    constructor() {
+        this.productos = [];
+        this.carrito = new Carrito();
+    }
+    agregarAlCarrito(producto) {
+        this.carrito.agregarAlCarrito(producto);
+    }
+    pagar() { }
+}
--- index.ts
+++ index.ts
@@ -14,8 +14,125 @@ let colores: Array<string> = ["Rojo", "Verde", "Azul"];
 let numerosYLetras: (number | string)[] = [1, "H", 3, "J"];
 let numerosYLetras2: Array<number | string> = [1, "H", 3, "J"];
 
-function saludar(nombre: string) {
+function saludar(nombre: string): void {
   console.log(`Hola ${nombre.toUpperCase()}!`);
 }
 
+function enviarMensaje(mensaje: string = "Valor por defecto") {
+  if (typeof mensaje === "string") {
+    mensaje.toUpperCase();
+  }
+}
+
+function sumar(a: number, b: number): number {
+  return a + b;
+}
+
+const restar = (a: number, b: number): number => a - b;
+
+// function despuesDe1Seg(accion: Function) {
+function despuesDe1Seg(accion: (a: number) => void) {
+  setTimeout(() => {
+    accion(5);
+  }, 1000);
+}
+
+despuesDe1Seg((a) => {
+  console.log(`Hola, recibimos: ${a}`);
+});
+
+enviarMensaje();
+
 saludar("Juan");
+
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+interface IAlumno {
+  nombre: string;
+  apellido: string;
+  edad: number;
+  email?: string;
+  estudiar: () => void;
+}
+
+let alumnos2: IAlumno[] = [];
+
+let alumnos: Array<IAlumno> = [
+  { nombre: "Naruto", apellido: "Uzumaki", edad: 12, estudiar: () => {} },
+  {
+    nombre: "Sasuke",
+    apellido: "Uchiha",
+    edad: 12,
+    email: "sasuke@gmail.com",
+    estudiar: () => {},
+  },
+];
+
+let sakura: IAlumno = {
+  nombre: "Sakura",
+  apellido: "Haruno",
+  edad: 12,
+  estudiar: () => {},
+};
+
+function actualizarAlumnoById(id: string, update: Partial<IAlumno>) {}
+
+function saludarAlumno(alumnoSinEdad: Omit<IAlumno, "edad">) {}
+
+export class Alumno {
+  nombre: string;
+  apellido: string;
+  edad: number;
+  email?: string;
+
+  constructor(nombre: string, apellido: string, edad: number, email?: string) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.email = email;
+  }
+
+  estudiar(): void {
+    console.log(`Hola, soy ${this.nombre} y estoy estudianto!`);
+  }
+}
+
+const rockLee = new Alumno("Rock", "Lee", 12, "rocklee@mail.com");
+const peterParker = new Alumno("Peter", "Parker", 15, "peter@mail.com");
+
+peterParker.estudiar();
+
+class Producto {
+  nombre: string;
+  precio: number;
+
+  constructor(nombre: string, precio: number) {
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+}
+
+class Carrito {
+  productos: Producto[] = [];
+
+  calcularTotal(): number {
+    return this.productos.reduce((prev, next) => prev + next.precio, 0);
+  }
+
+  agregarAlCarrito(producto: Producto) {
+    this.productos.push(producto);
+  }
+}
+
+class MyEcommerce {
+  productos: Producto[] = [];
+  carrito: Carrito = new Carrito();
+
+  agregarAlCarrito(producto: Producto) {
+    this.carrito.agregarAlCarrito(producto);
+  }
+
+  pagar() {}
+}
